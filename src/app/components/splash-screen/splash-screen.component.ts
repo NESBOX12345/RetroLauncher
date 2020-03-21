be332dac512184d8099c5c094358aa04a7fddb94
dannyTorres <dtorres@itexico.net>
2020-03-20 19:43:16 -0600

    Finished Splash Screen

--- src/app/components/splash-screen/splash-screen.component.ts
+++ src/app/components/splash-screen/splash-screen.component.ts
@@ -1,15 +1,91 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import {
+  trigger,
+  transition,
+  useAnimation,
+  group,
+  query,
+  animateChild
+} from '@angular/animations';
 import { Select } from '@ngxs/store';
 import { SettingsState } from 'src/app/shared/store/settings/settings.store';
 import { Observable, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 import { Lang } from 'src/app/shared/store/settings/types';
+import { FadeIn } from 'src/app/shared/animations/fade';
+import { Transform } from 'src/app/shared/animations/transform';
 
 @Component({
   selector: 'retro-splash-screen',
   templateUrl: './splash-screen.component.html',
-  styleUrls: ['./splash-screen.component.scss']
+  styleUrls: ['./splash-screen.component.scss'],
+  animations: [
+    trigger(
+      'showOnScreen',
+      FadeIn('void', '*', '500ms 100ms ease-in-out', [
+        '@translateLine1',
+        '@translateLine2',
+        '@translateLine3',
+        '@scaleTriangle',
+        '@displayLogo'
+      ])
+    ),
+    trigger(
+      'translateLine1',
+      Transform(
+        'void',
+        '*',
+        'translate(50px, 130px)',
+        '500ms 200ms ease-in-out'
+      )
+    ),
+    trigger(
+      'translateLine2',
+      Transform(
+        'void',
+        '*',
+        'rotate(60deg) translate(-6px, -76px)',
+        '500ms 200ms ease-in-out'
+      )
+    ),
+    trigger(
+      'translateLine3',
+      Transform(
+        'void',
+        '*',
+        'rotate(-60deg) translate(-62px, -73px)',
+        '500ms 200ms ease-in-out'
+      )
+    ),
+    trigger(
+      'scaleTriangle',
+      Transform(
+        'void',
+        '*',
+        'translate(0, 0) scale(0.6) rotate(180deg)',
+        '500ms 200ms ease-in-out'
+      )
+    ),
+    trigger(
+      'scaleViewport',
+      Transform(
+        'void',
+        '*',
+        'scale(0.8) translateX(80px)',
+        '500ms 200ms ease-in-out'
+      )
+    ),
+    trigger(
+      'displayLogo',
+      Transform(
+        'void',
+        '*',
+        'translateX(30px) skewX(30deg)',
+        '800ms 700ms ease-in-out'
+      )
+    )
+  ]
 })
 export class SplashScreenComponent implements OnInit, OnDestroy {
   @Select(SettingsState.lang) lang$: Observable<Lang>;
--- src/app/shared/animations/fade.ts
+++ src/app/shared/animations/fade.ts
@@ -1,15 +1,108 @@
-import { animation, style, animate } from '@angular/animations';
+import {
+  style,
+  animate,
+  AnimationMetadata,
+  state,
+  transition,
+  group,
+  query,
+  animateChild
+} from '@angular/animations';
 
-export const FadeIn = animation([
-  style({
-    opacity: 0
-  }),
-  animate('{{time}}', style({ opacity: 1 }))
-]);
+/**
+ * @param start - Transition Start Name
+ * @param end - Transition End Name
+ * @param time - Animation timing: `duration delay easing`
+ * @param childrens -- `Optional:` Specify a childrens animation triggers to run in parallel with the main animation.
+ * - Child trigger name must have `@` prefix.
+ * @example Usage Example:
+ *
+ * ```ts
+ * animations: [
+ *    trigger(
+ *      'triggerName',
+ *      FadeIn('void', '*', '1s 100ms ease-in-out', ['childTriggerName']))
+ * ]
+ * ```
+ *
+ * @returns AnimationMetadata[]
+ *
+ */
+export const FadeIn = (
+  start: string,
+  end: string,
+  time: string,
+  childrens: string[] = []
+): AnimationMetadata[] => {
+  return [
+    state(
+      start,
+      style({
+        opacity: 0
+      })
+    ),
+    state(
+      end,
+      style({
+        opacity: 1
+      })
+    ),
+    transition(`${start} => ${end}`, [
+      group([
+        ...childrens.map(child =>
+          query(child, animateChild(), { optional: true })
+        ),
+        animate(time, style({ opacity: 1 }))
+      ])
+    ])
+  ];
+};
 
-export const FadeOut = animation([
-  style({
-    opacity: 1
-  }),
-  animate('{{time}}', style({ opacity: 0 }))
-]);
+/**
+ * @param start - Transition Start Name
+ * @param end - Transition End Name
+ * @param time - Animation timing: 'duration delay easing'
+ * @param childrens -- `Optional:` Specify a childrens animation triggers to run in parallel with the main animation.
+ * - Child trigger name must have `@` prefix.
+ * @example Usage Example:
+ *
+ * ```ts
+ * animations: [
+ *    trigger(
+ *      'triggerName',
+ *      FadeOut('void', '*', '1s 100ms ease-in-out', ['childTriggerName']))
+ * ]
+ * ```
+ *
+ * @returns AnimationMetadata[]
+ *
+ */
+export const FadeOut = (
+  start: string,
+  end: string,
+  time: string,
+  childrens: string[] = []
+): AnimationMetadata[] => {
+  return [
+    state(
+      start,
+      style({
+        opacity: 1
+      })
+    ),
+    state(
+      end,
+      style({
+        opacity: 0
+      })
+    ),
+    transition(`${start} => ${end}`, [
+      group([
+        ...childrens.map(child =>
+          query(child, animateChild(), { optional: true })
+        ),
+        animate(time, style({ opacity: 0 }))
+      ])
+    ])
+  ];
+};
